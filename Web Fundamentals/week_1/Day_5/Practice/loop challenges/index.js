0d14d0ed0e32e3546aa22db77a32a2286454d2c8
abdennour1122 <ayediabdennour@gmail.com>
2024-02-05 14:54:11 +0100

    loop challenges

---
+++ Web Fundamentals/week_1/Day_5/Practice/loop challenges/index.js
@@ -0,0 +1,22 @@
+for(var i=1;i<=20;i++){
+    console.log(i)
+}
+for(var i=100;i>=1;i-=3){
+    console.log(i)
+}
+for(var i=-3.5;i<=4;i+=1.5){
+    console.log(i)
+}
+
+
+sum=0
+for(var i=1;i<=100;i++){
+    sum=sum+i
+    console.log("SUM",sum)
+}
+fact=1
+for(var i=1;i<=12;i++){
+    fact=fact*i
+    console.log(fact)
+}
+
