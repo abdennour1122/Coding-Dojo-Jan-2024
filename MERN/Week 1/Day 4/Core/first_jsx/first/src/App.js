1ee65c0cffcc828ff657b49f09b417e24974a34b
abdennour1122 <ayediabdennour@gmail.com>
2024-04-04 13:26:02 +0100

    first jsx

---
+++ MERN/Week 1/Day 4/Core/first_jsx/first/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello dojo !</h1>
+      <h3>things i need to do</h3>
+      <ul>
+        <li>learn react</li>
+        <li>climn mt everest</li>
+        <li>run a marathon</li>
+        <li>feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
