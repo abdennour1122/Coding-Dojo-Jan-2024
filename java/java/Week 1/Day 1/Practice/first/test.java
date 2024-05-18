8419c357d6b59c2e5f518efcdccc9b0fdc208d6c
abdennour1122 <ayediabdennour@gmail.com>
2024-05-18 22:51:24 +0100

    first

---
+++ java/java/Week 1/Day 1/Practice/first/test.java
@@ -0,0 +1,8 @@
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Bilel Laadhar");
+        System.out.println("I am 38 years old");
+        System.out.println("My hometown is Sfax, Tunisia");
+    }
+}
